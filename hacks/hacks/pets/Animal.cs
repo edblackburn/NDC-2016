5a99113f9cd910ddc2f8001aa91ba92953af6bd4
Ed Blackburn <ed@ejb.name>
2016-01-11 00:34:14 +0000

    Entities samples

---
+++ hacks/hacks/pets/Animal.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace hacks.pets
+{
+    abstract class Animal
+    {
+        protected internal TextWriter Writer { get; }
+
+        protected Animal(TextWriter writer)
+        {
+            Writer = writer;
+        }
+
+        internal abstract void Sound();
+
+        internal abstract short NumberOfLegs { get; }
+
+        internal virtual void Move()
+        {
+            for (var leg = 0; leg < NumberOfLegs; leg++)
+            {
+                Writer.WriteLine("Moved leg {0}", leg);
+            }
+        }
+    }
+
+    class Lion : Animal
+    {
+        public Lion() : base(Console.Out)
+        {
+            NumberOfLegs = 4;
+        }
+
+        internal override void Sound()
+        {
+            Writer.WriteLine("Roar");
+        }
+
+        internal override short NumberOfLegs { get; }
+    }
+
+    class Snake : Animal
+    {
+        public Snake() : base(Console.Out)
+        {
+        }
+
+        internal override void Sound()
+        {
+            Writer.WriteLine("Hiss");
+        }
+
+        internal override short NumberOfLegs { get; }
+
+        internal override void Move()
+        {
+            Writer.WriteLine("Slither");
+        }
+    }
+}
